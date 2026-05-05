605eaee298f8805223b529460a0bbba48f31cb24
Gael <angel.ga@northeastern.edu>
2026-05-04 22:40:36 -0500

    chore: move configuration logic

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,5 +1,12 @@
+from os import environ
+from aiohttp import ClientSession, ClientTimeout
+import pytest
 import logging
 from loguru import logger
+
+
+BASE_URL = "http://127.0.0.1:8080"
+SESSION_ID = environ["SESSION_ID"]
 
 
 class _PropagateHandler(logging.Handler):
@@ -9,3 +16,32 @@ class _PropagateHandler(logging.Handler):
 
 logger.remove()
 logger.add(_PropagateHandler(), format="{message}")
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="session")
+async def client():
+    cookies = {"session_id": SESSION_ID}
+    async with ClientSession(
+        base_url=BASE_URL, cookies=cookies, timeout=ClientTimeout(total=15)
+    ) as session:
+        yield session
+
+
+@pytest.fixture(scope="session")
+async def test_playlist(client: ClientSession):
+    """Creates a playlist at the start of the session and deletes it at the end."""
+    async with client.post("/playlists", json={"name": "pytest"}) as response:
+        response.raise_for_status()
+        playlist = await response.json()
+        logger.info(f"Created test playlist: {playlist['name']}")
+
+    yield playlist
+
+    async with client.delete(f"/playlists/{playlist['id']}") as response:
+        response.raise_for_status()
+        logger.info(f"Deleted test playlist: {playlist['name']}")
--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -5,44 +5,12 @@ Integration tests for API endpoints :)
 """
 
 import pytest
-from loguru import logger
-from os import environ
-from aiohttp import ClientSession, ClientTimeout, ClientResponseError
+from aiohttp import ClientSession, ClientResponseError
+from conftest import BASE_URL
 
-BASE_URL = "http://127.0.0.1:8080"
-SESSION_ID = environ["SESSION_ID"]
 
 TRACK_IDS = ["1oNYiuCvyixmwcyNZyq3Dd", "51vNSpNP76OEzvwVB7kIKT", "52wpFNuwZEr4Im7BSoo2vF"]  # fmt: skip
 TRACK_URIS = [f"spotify:track:{t_id}" for t_id in TRACK_IDS]
-
-
-@pytest.fixture(scope="session")
-def anyio_backend():
-    return "asyncio"
-
-
-@pytest.fixture(scope="session")
-async def client():
-    cookies = {"session_id": SESSION_ID}
-    async with ClientSession(
-        base_url=BASE_URL, cookies=cookies, timeout=ClientTimeout(total=15)
-    ) as session:
-        yield session
-
-
-@pytest.fixture(scope="session")
-async def test_playlist(client: ClientSession):
-    """Creates a playlist at the start of the session and deletes it at the end."""
-    async with client.post("/playlists", json={"name": "pytest"}) as response:
-        response.raise_for_status()
-        playlist = await response.json()
-        logger.info(f"Created test playlist: {playlist['name']}")
-
-    yield playlist
-
-    async with client.delete(f"/playlists/{playlist['id']}") as response:
-        response.raise_for_status()
-        logger.info(f"Deleted test playlist: {playlist['name']}")
 
 
 class TestAPI:
