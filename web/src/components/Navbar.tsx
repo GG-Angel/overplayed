afe52905fd4e1f79ff08e3cac07452bb39ac41e2
Gael <angel.ga@northeastern.edu>
2026-05-14 14:21:44 -0400

    feat: show playlist name in navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import { SkipForward } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AvatarControl from "./AvatarControl";
+import { usePlaylist } from "@/hooks/playlists";
 
-const Logo = () => (
-  <Link to="/" className="inline-flex items-center gap-1.5 select-none">
-    <SkipForward className="text-primary" />
-    <span className="text-lg font-semibold hidden sm:block">Overplayed</span>
-  </Link>
-);
+const Logo = () => {
+  const { playlistId } = useParams();
+  const { data: playlist } = usePlaylist(playlistId);
+
+  return (
+    <Link to="/" className="inline-flex items-center gap-1.5 select-none overflow-hidden">
+      <SkipForward className="text-primary" />
+      <span className="text-lg font-semibold hidden sm:block">Overplayed</span>
+      {playlist && (
+        <span className="text-muted-foreground truncate max-w-sm">/ {playlist.name}</span>
+      )}
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center py-2">
+    <div className="flex justify-between items-center py-2 gap-4">
       <Logo />
       <AvatarControl />
     </div>
