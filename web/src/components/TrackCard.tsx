3cdeaa24a1ab6d6eb2fed362d9b6aa669aa9c93d
Gael <angel.ga@northeastern.edu>
2026-05-14 14:32:55 -0400

    feat: resize card responsively

--- web/src/components/TrackCard.tsx
+++ web/src/components/TrackCard.tsx
@@ -10,7 +10,7 @@ const TrackCard = ({ track }: TrackCardProps) => {
   const artistList = track.artists.map((t) => t.name).join(", ");
 
   return (
-    <Card padding="square" className="flex flex-col gap-3 w-64 select-none">
+    <Card padding="square" className="flex flex-col gap-3 w-64 sm:w-72 select-none">
       <img
         className="aspect-square object-cover w-full rounded-sm"
         src={coverUrl}
