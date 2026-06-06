0cd08687de6a40cebe2b72f8c2f72f81e01775da
Gael <angel.ga@northeastern.edu>
2026-06-06 03:27:56 -0500

    feat: add searchbar to playlist selection

--- web/src/app/pages/playlists/selection.tsx
+++ web/src/app/pages/playlists/selection.tsx
@@ -2,27 +2,50 @@ import PlaylistCard from "@/features/playlist/components/PlaylistCard";
 import { useNavigate } from "react-router-dom";
 import LoadingState from "@/components/states/LoadingState";
 import { useUserPlaylists } from "@/features/playlist/api/get-playlists";
+import { Search } from "lucide-react";
+import { useMemo, useState } from "react";
+
+const DEFAULT_SEARCH_QUERY = "";
 
 const SelectionPage = () => {
   const { data: playlists, isLoading } = useUserPlaylists();
   const navigate = useNavigate();
 
-  const navigateToPlaylist = (playlistId: string) => {
-    navigate(`${playlistId}/swipe`);
-  };
+  const [searchQuery, setSearchQuery] = useState(DEFAULT_SEARCH_QUERY);
+
+  const searchedPlaylists = useMemo(() => {
+    if (!playlists) return [];
+    const normalizedQuery = searchQuery.trim().toLowerCase();
+    return playlists.filter((p) => p.name.toLowerCase().includes(normalizedQuery));
+  }, [playlists, searchQuery]);
 
   if (isLoading) return <LoadingState message="Loading playlists..." />;
 
   return (
-    <>
-      <p className="text-4xl text-center font-medium mb-4">Select a Playlist</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-6 pb-4">
-        {playlists &&
-          playlists.map((p) => (
-            <PlaylistCard key={p.id} playlist={p} onClick={navigateToPlaylist} />
-          ))}
+    <div className="flex flex-col gap-6">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl tracking-tighter font-bold text-center">
+        Select a Playlist
+      </h1>
+      <div className="flex items-center gap-2 bg-card border-card-border focus-within:border-muted-foreground border-2 py-2 px-3 rounded-md">
+        <Search className="text-muted-foreground" />
+        <input
+          className="w-full outline-none"
+          placeholder="Search for a playlist"
+          type="search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
       </div>
-    </>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-6 pb-4">
+        {searchedPlaylists.map((p) => (
+          <PlaylistCard
+            key={p.id}
+            playlist={p}
+            onClick={(playlistId) => navigate(`${playlistId}/swipe`)}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
