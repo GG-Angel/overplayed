f8c79d31a20c2e594b77882afa687397acf693a9
Gael <angel.ga@northeastern.edu>
2026-06-14 01:02:48 -0500

    fix: user manager done

---
+++ queue/app/state.py
@@ -0,0 +1,6 @@
+from user_management import UserManager
+
+
+class State:
+    def __init__(self, user_manager: UserManager):
+        self.user_manager = user_manager
--- queue/app/user_management.py
+++ queue/app/user_management.py
@@ -1,33 +1,66 @@
 import asyncio
+from datetime import datetime
+from pydantic import BaseModel
 from aiohttp import ClientSession
 from core.settings import settings
 
-# removing users:
-# DELETE https://developer.spotify.com/api/ws4d/warp/clients/<client_id>/users/id/<id>
-# no body
 
-# adding users:
-# POST https://developer.spotify.com/api/ws4d/warp/clients/<client_id>/users
-# body params (JSON):
-# clientId	"app client id"
-# email	"user email"
-# name	"full name"
+class NewUser(BaseModel):
+    name: str
+    email: str
 
 
-async def get_users(session: ClientSession):
-    async with session.get(
-        f"/api/s4d/warp/clients/{settings.spotify_client_id}/users"
-    ) as response:
-        response.raise_for_status()
-        print(await response.json())
+class User(BaseModel):
+    id: str
+    name: str
+    email: str
+    clientId: str
+    createdAt: datetime
+    needsAnonymization: bool
+
+
+class GetUsersResponse(BaseModel):
+    users: list[User]
+
+
+class UserManager:
+    def __init__(self, session: ClientSession, client_id: str):
+        self._session = session
+        self._client_id = client_id
+
+    async def get_users(self) -> list[User]:
+        async with self._session.get(
+            f"/api/s4d/warp/clients/{self._client_id}/users"
+        ) as response:
+            return GetUsersResponse.model_validate(await response.json()).users
+
+    async def add_user(self, user: NewUser) -> User:
+        async with self._session.post(
+            f"/api/ws4d/warp/clients/{self._client_id}/users",
+            json=user.model_dump(),
+        ) as response:
+            return User.model_validate(await response.json())
+
+    async def remove_user(self, user_id: str):
+        await self._session.delete(
+            f"/api/ws4d/warp/clients/{self._client_id}/users/id/{user_id}"
+        )
 
 
 async def main():
     async with ClientSession(
         base_url="https://developer.spotify.com",
         headers={"Authorization": f"Bearer {settings.spotify_bearer_token}"},
+        raise_for_status=True,
     ) as session:
-        await get_users(session)
+        manager = UserManager(client_id=settings.spotify_client_id, session=session)
+
+        print("adding user")
+        new_user = NewUser(name="Joe Mama", email="idontexist@gmail.com")
+        print(await manager.add_user(new_user))
+
+        print("getting users")
+        print(await manager.get_users())
 
 
 if __name__ == "__main__":
