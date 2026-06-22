5284b5fd597f5e8145d35fedf2adce1a497891b9
Gael <angel.ga@northeastern.edu>
2026-06-22 02:43:04 -0500

    working validator

--- queue/app/spotify/validate.py
+++ queue/app/spotify/validate.py
@@ -1,31 +1,35 @@
 import json
-from bs4 import BeautifulSoup
 import asyncio
-from loguru import logger
+from bs4 import BeautifulSoup
 from aiohttp import ClientSession
 
 
 class UserValidator:
     def __init__(self, session: ClientSession):
         self.session = session
+        self.api_key: str | None = None
 
     async def does_user_exist(self, email: str) -> bool:
-        async with self.session.get(
+        async with self.session.post(
             "https://spclient.wg.spotify.com/signup/public/v2/account/validate",
             json={
                 "fields": [{"field": "FIELD_EMAIL", "value": email}],
-                "client_info": {"api_key": "..."},
+                "client_info": {"api_key": self.api_key},
             },
         ) as response:
             data = await response.json()
-            if "success" in data:
-                return True
-
             if "error" not in data:
-                logger.warning("Bad response did not contain an error block.")
                 return False
 
-            return True  # TODO: fix later
+            error = data["error"]
+            if "already_exists" in error:
+                return True
+
+            if "field_errors" in error.get("invalid_argument", {}):
+                raise ValueError("Invalid email.")
+
+            self.api_key = await self._fetch_api_key()
+            return await self.does_user_exist(email)
 
     async def _fetch_api_key(self) -> str:
         async with self.session.get("https://www.spotify.com/us/signup") as response:
@@ -33,7 +37,7 @@ class UserValidator:
 
             script_tag = soup.find("script", id="__NEXT_DATA__")
             if not script_tag:
-                raise RuntimeError("No tag found with id __NEXT_DATA__")
+                raise RuntimeError("No script tag found with id __NEXT_DATA__")
 
             data = json.loads(script_tag.text)
             return data["props"]["pageProps"]["keys"]["signupServiceAppKey"]
@@ -42,8 +46,7 @@ class UserValidator:
 async def main():
     async with ClientSession() as session:
         validator = UserValidator(session)
-        key = await validator._fetch_api_key()
-        print(key)
+        print(await validator.does_user_exist("example@gmail.com"))
 
 
 if __name__ == "__main__":
