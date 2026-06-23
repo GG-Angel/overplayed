63127b405bed9be4ba4fd4899f7f5d2a26174515
Gael <angel.ga@northeastern.edu>
2026-06-23 03:17:24 -0500

    delete backup

--- queue/app/main.py
+++ queue/app/main.py
@@ -54,3 +54,26 @@ def healthcheck():
 @app.get("/queue")
 async def get_queue(state: State = Depends(get_state)):
     return await state.users.get_users()
+
+
+# @router.get("/queue")
+# async def get_queue(
+#     request: Request,
+#     state: State = Depends(get_state),
+# ) -> QueueDetails:
+#     users = await state.users.get_users()
+#     queue_size = await state.queue.get_size()
+#     return QueueDetails(
+#         active_users=len(users),
+#         available_slots=max(0, USER_LIMIT - len(users)),
+#         queue_size=queue_size,
+#     )
+
+
+# @router.post("/queue")
+# async def enqueue_user(
+#     request: Request,
+#     user: NewUser,
+#     state: State = Depends(get_state),
+# ) -> QueuePosition:
+#     return QueuePosition(position=await state.queue.enqueue(user))
