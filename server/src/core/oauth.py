08368c9d58468e0ce2579598fae41f1a905161f1
Gael <angel.ga@northeastern.edu>
2026-08-14 00:05:45 -0400

    Renamed

--- server/src/core/oauth.py
+++ server/src/core/oauth.py
@@ -4,12 +4,12 @@ from spotipy.cache_handler import CacheHandler
 from settings import settings
 
 
-class _NoOpCacheHandler(CacheHandler):
+class NoOpCacheHandler(CacheHandler):
     def get_cached_token(self) -> None:
         return None
 
     def save_token_to_cache(self, token_info: dict[str, object]) -> None:
-        pass
+        return None
 
 
 def build_spotify_oauth() -> SpotifyOAuth:
@@ -18,5 +18,5 @@ def build_spotify_oauth() -> SpotifyOAuth:
         client_secret=settings.spotify_client_secret,
         redirect_uri=settings.callback_url,
         scope=settings.spotify_scope,
-        cache_handler=_NoOpCacheHandler(),
+        cache_handler=NoOpCacheHandler(),
     )
